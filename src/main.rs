68ee40a16b5b0fffdef31a64d14a8433ba3dbff6
snakagomi <tanakagoro0319@yahoo.co.jp>
2023-07-08 09:41:44 +0900

    modify
    Sequences were converted to vectors.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
 use rand::seq::SliceRandom;
 
 fn main() {
-    let mut nums = [0; 75];
+    let mut nums = vec![];
     for i in 1..=75 {
-        nums[i - 1] = i;
+        nums.push(i);
     }
 
     let mut rng = rand::thread_rng();
     nums.shuffle(&mut rng);
 
-    for y in 0..5 {
-        for x in 0..5 {
-            let i = y * 5 + x;
-            if i == 12 {
-                print!("  *,");
-            } else {
-                print!("{:3},", nums[i]);
-            }
+    for i in 0..25 {
+        if i == 12 {
+            print!("  *,");
+        } else {
+            print!("{:3},", nums[i]);
         }
-        println!("");
+
+        if i % 5 == 4 {
+            println!("");
+        }
     }
 }
